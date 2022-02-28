70b8964ae212502c3f0e31ff6daa1197fbc1d51b
Devikasarath77 <47816479+Devikasarath77@users.noreply.github.com>
2022-02-28 22:56:23 +0530

    Add files via upload

--- DuplicateLead.java
+++ DuplicateLead.java
@@ -1,11 +1,14 @@
 package week2.day2;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DuplicateLead {
+
+	
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
@@ -28,38 +31,61 @@ public class DuplicateLead {
 		// step 8 -- pending
 
 		// step 9 Enter Email
+		
 		driver.findElement(By.xpath("//span[text()='Email']")).click();
 		driver.findElement(By.xpath("//input[@name='emailAddress']")).sendKeys("devikask7@gmail.com");
 
 		// step10 find button
 		driver.findElement(By.xpath("//button[contains(text(),'Find Leads')]")).click();
-
-		 driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-firstName'])[1]")).getText();
+		Thread.sleep(2000);
 
 		// step11 get the firstleading text
-//
-//		 String getFirstleadingText = driver.findElement(By.xpath("\"(//div[@class='x-grid3-cell-inner x-grid3-col-firstName'])[1]\"")).getText();
-// System.out.println(" Capture name of First Resulting lead" +getFirstleadingText);
+		
+		
+	 String getFirstleadingText = driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-firstName'])[1]")).getText();
+System.out.println(" Capture name of First Resulting lead  " +getFirstleadingText);
 
 		// step12 -- click on the first leading
 		 
-		 Thread.sleep(3000);
+		 Thread.sleep(5000);
 		 
-		 driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-firstName'])[1]")).click();
-//		 getFirstleadingText.click();
+	WebElement clickFirstData = driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-firstName'])[1]/a"));
+	clickFirstData.click();
+	 
+	 
+
 		// step13 Click Duplicate Lead
 
-//		 driver.findElement(By.xpath(" //a[contains(text(),'Duplicate Lead')]")).click();
+	 driver.findElement(By.xpath(" //a[contains(text(),'Duplicate Lead')]")).click();
 
 		// step14 Verify the title as 'Duplicate Lead'
-
+	 
+	String getPageText =  driver.findElement(By.xpath("//div[contains(text(),'Duplicate Lead')]")).getText();
+	 System.out.println("title of current page :" +getPageText);
 		// step15 Click Create Lead
 
-//		 driver.findElement(By.xpath("//input[@name='submitButton']")).click();
+		 driver.findElement(By.xpath("//input[@name='submitButton']")).click();
 		// step16 Confirm the duplicated lead name is same as captured name
-
+		 
+//		 String getCaptureName = driver.findElement(By.xpath("//input[@id='createLeadForm_firstName']")).getText();
+//		 System.out.println("get the firstname "+getCaptureName);
+		 
+		 
+		 Thread.sleep(2000);
+		 String elementfirstName = driver.findElement(By.xpath("//span[@id='viewLead_firstName_sp']")).getText();
+//			elementfirstName.getText();
+			System.out.println(" "+elementfirstName);
+			
+if (elementfirstName.contains("Devika"))
+{ 
+	System.out.println("test case passed ");
+}
+	else {
+		System.out.println("testcase failed ");
+}
+		 
 		// step 17 Close the browser (Do not log out)
-		// driver.close();
+		driver.close();
 
 	}
 
